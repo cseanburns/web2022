3ad5b7db64d35f9e81f7300c72adfd4f6683ce19
C. Sean Burns <sean.csb@gmail.com>
2022-04-12 15:22:28 -0400

    added comment and changed hours

--- scripts/main.js
+++ scripts/main.js
@@ -65,21 +65,21 @@ https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model/Introduct
 //console.log(sections);
 //sections[1].style.border = '1px solid black';
 
-// Start dark mode if it's before 6pm
-if (new Date().getHours() > 15) {
+// Start dark mode if it's after 6:59:59pm
+if (new Date().getHours() > 18) {
   //alert(new Date().getHours());
   const html = document.querySelector('html');
   html.style.backgroundColor = 'black';
   html.style.color = 'white';
 }
 
-if (new Date().getHours() > 15) {
+if (new Date().getHours() > 18) {
   const wrapperBody = document.getElementsByClassName('wrapper');
   wrapperBody[0].style.backgroundColor = 'black';
   wrapperBody[0].style.color = 'white';
 }
 
-if (new Date().getHours() > 15) {
+if (new Date().getHours() > 18) {
   const secondaryBody = document.getElementById('secondary');
   secondaryBody.style.backgroundColor = 'black';
   secondaryBody.style.color = 'white';
